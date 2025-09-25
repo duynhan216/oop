fc2bb871467bfbbdeb62c33a58c1db843ddd9460
Nguyen Le Duy Nhan <nguyenleduynhan06@gmail.com>
2025-09-25 19:02:35 +0700

    update

--- oop-main/HW1/SumOfTwoDice.java
+++
@@ -1,8 +0,0 @@
-public class SumOfTwoDice {
-    public static void main(String[] args) {
-        int Sides = 6;
-        int a = 1 + (int) (Math.random() * Sides);
-        int b = 1 + (int) (Math.random() * Sides);
-        System.out.println(a + b);
-    }
-}
